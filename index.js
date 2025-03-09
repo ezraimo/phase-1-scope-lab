ba7263fe1d68296018636e9b085bf13ada07058f
EZRA KIPRUTO SEREM <ezraserem4038@gmail.com>
2025-03-09 16:08:14 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare customerName globally
+var customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Implicitly global variable
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+// Declare a constant for least favorite customer
+const leastFavoriteCustomer = 'someone';
+
+// Function to change leastFavoriteCustomer (which should fail)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'another person'; // This will throw an error
+}
